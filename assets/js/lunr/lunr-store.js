508f982458ce37a9e021985b84130088361faa46
byerdk <byerdk@users.noreply.github.com>
2025-04-23 13:44:19 +0000

    deploy: 49586c36bf9b8633a0031df7a9d729c736ff7d5f

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,79 @@
+var store = [{
+        "title": "Page Not Found",
+        "excerpt":"Sorry, but the page you were trying to view does not exist.  ","categories": [],
+        "tags": [],
+        "url": "/404.html",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"Tempor velit sint sunt ipsum tempor enim ad qui ullamco. Est dolore anim ad velit duis dolore minim sunt aliquip amet commodo labore. Ut eu pariatur aute ea aute excepteur laborum. Esse ea esse excepteur minim mollit qui cillum excepteur ex dolore magna. Labore deserunt fugiat incididunt incididunt sint ea....","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Posts by Category",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/categories/",
+        "teaser": null
+      },{
+        "title": "Reimagining the Future of Work—With Data, Insight, and Purpose",
+        "excerpt":"Welcome to DBSDS—Dan Byer Social Data Science I’m Dan Byer: a retired Army Major, sociologist-in-training, and social data scientist. I use modern data tools to tackle social questions, support research, and inform public decision-making. This site showcases my work at the intersection of: 📊 Data science and sociology 🏛️ Civic...","categories": [],
+        "tags": [],
+        "url": "/",
+        "teaser": null
+      },{
+        "title": "Posts by Tag",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/tags/",
+        "teaser": null
+      },{
+        "title": "Posts by Year",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/posts/",
+        "teaser": null
+      },{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/2010/01/07/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/blog/2010/01/07/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "tags": ["chat","Post Formats"],
+        "url": "/blog/2010/01/08/post-chat/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "tags": ["Post Formats","notice"],
+        "url": "/blog/2010/02/05/post-notice/",
+        "teaser": null
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "tags": ["Post Formats","quote"],
+        "url": "/blog/2010/02/05/post-quote/",
+        "teaser": null
+      },{
+        "title": "Post: Link",
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Blog"],
+        "tags": ["link","Post Formats"],
+        "url": "/blog/2010/03/07/post-link/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "/blog/2019/04/18/welcome-to-jekyll/",
+        "teaser": null
+      }]
